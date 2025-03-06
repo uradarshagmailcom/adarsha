0f3a5720dc85b4c6782c78aad0ed751e521f4370
uradarshagmailcom <58351518+uradarshagmailcom@users.noreply.github.com>
2025-03-06 09:26:58 +0530

    Add files via upload

---
+++ assignment_1a.py
@@ -0,0 +1,12 @@
+A = input("Enter the First number : ")
+A = float(A)
+B = input("Enter the Second number : ")
+B = float(B)
+C = A+B
+print("Addition : ", C)
+D = A-B
+print("Subtraction : ", D)
+E = A*B
+print("Multiplication : ", E)
+F = A / B
+print("Division : ", F)
---
+++ assignment_1b.py
@@ -0,0 +1,3 @@
+FN = input('Enter your First Name: ')
+SN = input('Enter your Second Name: ')
+print('Hello ',FN + " " +  SN,'Welcome to the python program ')
